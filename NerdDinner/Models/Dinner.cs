bd5b7566bc9bde564fa20aa5ede4376221882c10
saians <saifullah.ansari@gmail.com>
2014-07-20 19:16:44 +0200

    first commit

---
+++ NerdDinner/Models/Dinner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NerdDinner.Models
+{
+    public partial class Dinner
+    {
+        public bool IsValid
+        {
+            get { return (GetRuleViolations().Count() == 0); }
+        }
+
+        private IEnumerable<RuleViolation> GetRuleViolations()
+        {
+            yield break;
+        }
+
+        partial void OnValidate(System.Data.Linq.ChangeAction action)
+        {
+            if (!IsValid)
+                throw new ApplicationException("Rule violation prevent saving");
+        }
+
+    }
+
+    public class RuleViolation
+    {
+        public string ErrorMessage { get; private set; }
+        protected string PropertyName { get; set; }
+
+        public RuleViolation(string errorMessage)
+        {
+            ErrorMessage = errorMessage;
+        }
+
+        public RuleViolation(string errorMessage, string propertyName)
+        {
+            ErrorMessage = errorMessage;
+            PropertyName = propertyName;
+        }
+    }
+}
---
+++ NerdDinner/Models/DinnerRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace NerdDinner.Models
+{
+    public class DinnerRepository
+    {
+        private NerdDinnerDataContext db = new NerdDinnerDataContext();
+
+
+        //QUERY METHODS
+
+        public IQueryable<Dinner> FindAllDinner()
+        {
+            return db.Dinners;
+        }
+
+        public IQueryable<Dinner> FindUpcomingDinners()
+        {
+            return from dinner in db.Dinners
+                   where dinner.EventDate > DateTime.Now
+                   orderby dinner.EventDate
+                   select dinner;
+        }
+
+        public Dinner GetDinnner(int id)
+        {
+            return db.Dinners.SingleOrDefault(d => d.DinnerID == id);
+        }
+
+        //INSERT / DELETE METHODS
+
+        public void Add(Dinner dinner)
+        {
+            db.Dinners.InsertOnSubmit(dinner);
+        }
+
+        public void Delete(Dinner dinner)
+        {
+            db.RSVPs.DeleteAllOnSubmit(dinner.RSVPs);
+            db.Dinners.DeleteOnSubmit(dinner);
+        }
+
+        //PERSISTENCE
+
+        public void Save()
+        {
+            db.SubmitChanges();
+        }
+
+    }
+}
